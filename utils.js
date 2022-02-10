f6066b3987826a3b72632809cb1475f206ede932
Fransiskus Febryan Suryawan <franfebryan@protonmail.com>
2022-02-10 22:32:02 +0700

    feat: initial commit, colorful square

---
+++ utils.js
@@ -0,0 +1,24 @@
+const Util = {
+  createShader: (gl, type, source) => {
+    const shader = gl.createShader(type);
+    gl.shaderSource(shader, source);
+    gl.compileShader(shader);
+    const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+    if(success) return shader;
+
+    console.error(gl.getShaderInfoLog(shader));
+    gl.deleteShader(shader);
+  },
+
+  createProgram: (gl, vertexShader, fragmentShader) => {
+    const program = gl.createProgram();
+    gl.attachShader(program, vertexShader);
+    gl.attachShader(program, fragmentShader);
+    gl.linkProgram(program);
+    const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+    if(success) return program;
+
+    console.error(gl.getProgramInfoLog(program));
+    gl.deleteProgram(program);
+  }
+};
